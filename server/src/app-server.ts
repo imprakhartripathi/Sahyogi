87136fb51460ee92e74226d6c1a44e74de30c517
Prakhar Tripathi <imprakhartripathiofficial@gmail.com>
2025-02-05 20:59:50 +0530

    mongodb

--- server/src/app-server.ts
+++ server/src/app-server.ts
@@ -1,16 +1,29 @@
 import express from "express";
+import mongoose from "mongoose";
 import path from "path";
 import cors from "cors";
 import bodyParser from "body-parser";
 import router from "./app-router";
 import { clientPort, serverPort } from "./serversettings";
 
+const __dirname = path.resolve();
+const mongoDBurl = "mongodb://localhost:27017/miniproject2DB";
 const app = express();
+
 app.use(cors({ credentials: true, origin: [clientPort] }));
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "static")));
 app.use(router);
+
+mongoose
+  .connect(mongoDBurl)
+  .then(() => {
+    console.log("MongoDB Connection Successful");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
 
 app.listen(serverPort, () => {
   console.log(
