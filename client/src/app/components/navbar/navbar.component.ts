5320c0a00a8fcd2f52fdb2a7a4e225902a144100
Prakhar Tripathi <imprakhartripathiofficial@gmail.com>
2025-02-17 20:27:00 +0530

    user decoding

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { GetCurrentUserService } from '../../services/get-current-user.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: false,
-  
+
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
-
+  
 }
--- server/src/app-router.ts
+++ server/src/app-router.ts
@@ -3,9 +3,12 @@ const router = express.Router();
 
 import signupController from "./controllers/signupController";
 import loginController from "./controllers/loginController";
+import getCurrentUserController from "./controllers/getCurrentUserController";
 
 router.post('/signup', signupController);
 
 router.post('/login', loginController);
+
+router.get("/getcurusr", getCurrentUserController)
 
 export default router;
