d2b362d7093b05d069004fa43d27b79a4e6c1d54
Prakhar Tripathi <imprakhartripathiofficial@gmail.com>
2025-04-14 19:29:04 +0530

    Update serversettings.ts

--- server/src/serversettings.ts
+++ server/src/serversettings.ts
@@ -1,2 +1,2 @@
 export const serverPort = 4200;
-export const clientPort = "http://localhost:5000";
+export const clientPort = "http://localhost:5000", "https://e788-2401-4900-1c63-41c0-9c21-41e9-edf6-d357.ngrok-free.app";
