c6ca9ad29aa58fe076c8d8c69082bc1b52d13338
Prakhar Tripathi <imprakhartripathiofficial@gmail.com>
2025-04-14 20:49:37 +0530

    rrrrr

--- server/src/serversettings.ts
+++ server/src/serversettings.ts
@@ -1,3 +1,3 @@
 export const serverPort = 4200;
 export const localClientPort = "http://localhost:5000";
-export const ngrokPort = "https://solid-factually-donkey.ngrok-free.app/"
+export const ngrokPort = "https://solid-factually-donkey.ngrok-free.app"
