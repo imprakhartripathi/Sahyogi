b1372ccca212ce4d3bc428bcb64643f3425c3911
Prakhar Tripathi <imprakhartripathiofficial@gmail.com>
2025-04-14 19:33:15 +0530

    port changes

--- server/src/serversettings.ts
+++ server/src/serversettings.ts
@@ -1,2 +1,3 @@
 export const serverPort = 4200;
-export const clientPort = "http://localhost:5000";
+export const localClientPort = "http://localhost:5000";
+export const ngrokPort = "https://e788-2401-4900-1c63-41c0-9c21-41e9-edf6-d357.ngrok-free.app"
