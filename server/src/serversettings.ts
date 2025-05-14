1ad96fa410b57bd87bfaf345884095d686eebd82
Prakhar Tripathi <imprakhartripathiofficial@gmail.com>
2025-05-14 16:15:11 +0530

    another port change

--- server/src/serversettings.ts
+++ server/src/serversettings.ts
@@ -1,3 +1,3 @@
 export const serverPort = 4200;
-export const localClientPort = "http://localhost:6900";
+export const localClientPort = "http://localhost:3000";
 export const ngrokPort = "https://solid-factually-donkey.ngrok-free.app"
